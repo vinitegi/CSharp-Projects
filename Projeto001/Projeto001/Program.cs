7e5cdb1d29ab38a8f88710109c642c57b0bc69c3
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-02-12 13:46:03 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto001/Projeto001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
