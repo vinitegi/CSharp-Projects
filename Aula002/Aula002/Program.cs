429b233496bff1480ec2f331c93f1353c6f9897b
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-02-12 15:11:17 -0300

    commit New

--- Aula002/Aula002/Program.cs
+++ Aula002/Aula002/Program.cs
@@ -6,9 +6,31 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            byte n1 = 1;
+            byte n1 = 126;
+            int n2 = 321;
+            int n3 = 2147483647;
+            long n4 = 2147483648L; //sempre colocar um L ao final do valor de uma variavel do tipo 'long' para simbolizar o tipo da variavel
+            float n5 = 4.5f; //obrigatorio colocar o 'f' ao final do valor da variavel, para simbolizar que se trata de uma variavel do tipo 'float' e nao do tipo 'double'
+            double n6 = 4.5;
+
+            /*
+             * MUDANÇA TIPO DE VARIALVEL
+             */
+
+            bool completo = false; //valor verdade, veradeiro ou falso (0 e 1)
+            char caracter = 'a';
+            string nome = "otegi";
 
             Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(completo);
+            Console.WriteLine(caracter);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+           
         }
     }
 }
