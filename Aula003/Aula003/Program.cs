4b830e60607ac76a99148e41e72e56ba939e7295
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-02-19 10:18:27 -0300

    commit again

--- Aula003/Aula003/Program.cs
+++ Aula003/Aula003/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("---------------------------------------------------------------");
+﻿using System.Globalization;
+
+Console.WriteLine("---------------------------------------------------------------");
 
 char genero = 'F';
 int idade = 32;
@@ -8,4 +10,15 @@ string nome = "maria";
 Console.WriteLine(genero);
 Console.WriteLine(idade);
 Console.WriteLine(saldo.ToString("F2"));
+Console.WriteLine(saldo.ToString("F2", CultureInfo.InvariantCulture));
 Console.WriteLine(nome);
+Console.WriteLine("---------------------------------------------------------------");
+
+//PlaceHolders
+Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2}", nome, idade, saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+//Interpolacao
+Console.WriteLine($"{nome} tem {idade} e saldo igual a {saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+
+//Contacatenacao de strings
+Console.WriteLine(nome + " tem " + idade + " e saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture));
