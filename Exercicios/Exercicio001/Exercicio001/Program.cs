4e2530e561df52c6fb2d66101acc8ff0ef3e18a7
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-02-20 14:21:01 -0300

    second commit

--- Exercicios/Exercicio001/Exercicio001/Program.cs
+++ Exercicios/Exercicio001/Exercicio001/Program.cs
@@ -9,4 +9,15 @@ char genero = 'M';
 
 double preco1 = 2100.0;
 double preco2 = 650.50;
-double medida = 53.234567;  
+double medida = 53.234567;
+
+Console.WriteLine("Produtos: ");
+Console.WriteLine($"Computador, cujo o preco eh R$ {preco1}");
+Console.WriteLine($"Mesa de escritorio, cujo o preco eh R$ {preco2}");
+Console.WriteLine("\n");
+
+Console.WriteLine($"Registro: {idade} anos de idade, codigo {codigo} e genero: '{genero}'");
+
+Console.WriteLine($"Medida com oito casas descimais: {medida:F8}");
+Console.WriteLine($"Arredondando para tres casas: {medida:F3}");
+Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
