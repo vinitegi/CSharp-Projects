2025cc9416626e4604954766ba1f28ba82eca24b
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-02-20 14:02:34 -0300

    initial commit

---
+++ Exercicios/Exercicio001/Exercicio001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+string produto1 = "Computador";
+string produto2 = "Mesa de Escritorio";
+
+byte idade = 30;
+int codigo = 5900;
+char genero = 'M';
+
+double preco1 = 2100.0;
+double preco2 = 650.50;
+double medida = 53.234567;  
