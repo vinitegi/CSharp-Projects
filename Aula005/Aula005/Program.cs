4e620ce258d884f1c330323ce84e2616ff4c1c81
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-02-26 14:56:21 -0300

    initial commit

---
+++ Aula005/Aula005/Program.cs
@@ -0,0 +1,15 @@
+﻿//leitura de dados
+
+string frase = Console.ReadLine();
+
+Console.WriteLine(frase);
+
+string s = Console.ReadLine();
+string[] vet = s.Split(' ');
+string p1 = vet[0];
+string p2 = vet[1];
+string p3 = vet[2];
+
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+Console.WriteLine(p3);
