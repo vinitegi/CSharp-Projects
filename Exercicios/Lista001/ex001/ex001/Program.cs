9983fcd50115dee8bb2773931ad4e798dcf00a46
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-03 17:14:05 -0300

    initial commit

--- Exercicios/Lista001/ex001/ex001/Program.cs
+++ Exercicios/Lista001/ex001/ex001/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System.Globalization;
+
+Console.WriteLine("Digite os valores de 'A' e de 'B'");
+
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Soma = {0}", n1 + n2);
+
