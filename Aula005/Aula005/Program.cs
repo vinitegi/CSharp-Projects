847e165abfbd71827af502ccf8e50cb7d10374a8
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-03 14:56:39 -0300

    second commit

--- Aula005/Aula005/Program.cs
+++ Aula005/Aula005/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Globalization;
 
+
 //leitura de dados em string
+
 
 string frase = Console.ReadLine();
 
@@ -16,12 +18,24 @@ Console.WriteLine(p2);
 Console.WriteLine(p3);
 
 //leitura de dados de numeros e char
+//'Console.ReadLine()' faz a leitura apenas de strings. Portanto, usa-se 'tipoDaVariavel.Parse(Console.ReadLine()' para a leitura de outros tipos de variaveis
 
-int n1 = int.Parse(Console.ReadLine());
-char ch = char.Parse(Console.ReadLine());
-double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//int n1 = int.Parse(Console.ReadLine());
+//char ch = char.Parse(Console.ReadLine());
+//double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-Console.WriteLine(n1);
-Console.WriteLine(ch);
-Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+//teste pratico
+string[] str = Console.ReadLine().Split(' ');
+string nome = str[0];
+char sexo = char.Parse(str[1]);
+int idade = int.Parse(str[2]);
+double altura = double.Parse(str[3], CultureInfo.InvariantCulture);
 
+//Console.WriteLine(n1);
+//Console.WriteLine(ch);
+//Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+
+Console.WriteLine(nome);
+Console.WriteLine(sexo);
+Console.WriteLine(idade);
+Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
