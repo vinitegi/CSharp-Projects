57ad1e1b6dc82fc83cceafdde480a8dc54128671
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-05 13:51:37 -0300

    initial commit

---
+++ Exercicios/Lista001/ex003/ex003/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+Console.WriteLine("Digite os valores de A | B | C | D");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"A diferença do produto de A e B pelo produto de C e D: {a * b - c * d}");
