4d96bdf5eab737de2f3a059750bf1ac85cf54424
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-05 14:16:17 -0300

    initial commit

---
+++ Exercicios/Lista001/ex004/ex004/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Type your ID:");
+    int ID = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How many hours you worked last month?");
+    double hours = double.Parse(Console.ReadLine());
+
+Console.WriteLine("How much you get paid per hour?");
+    double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine($"Welcome back Worker Number {ID}");
+Console.WriteLine($"Your salary is USD${(hours * salary).ToString("F2", CultureInfo.InvariantCulture)}");
