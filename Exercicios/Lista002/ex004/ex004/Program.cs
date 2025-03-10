7dcbde0654b77c8e05a1dd47a246fceb4ba201a4
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-10 14:29:28 -0300

    initial commit

---
+++ Exercicios/Lista002/ex004/ex004/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite a hora inicial e a hora final da sua jogatina:");
+
+string[] horarios = Console.ReadLine().Split(' ');
+int inicio = int.Parse(horarios[0]);
+int final = int.Parse(horarios[1]);
+int duracao;
+
+if (inicio < final)
+{
+    duracao = inicio - final;
+}
+else
+{
+    duracao = 24 - inicio + final;
+}
+
+Console.WriteLine($"Voce jogou por {duracao}");
