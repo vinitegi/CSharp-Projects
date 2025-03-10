9d31e2a197779a5e54e642a5b0896fb1f71d0241
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-10 13:54:58 -0300

    initial commit

---
+++ Exercicios/Lista002/ex002/ex002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Type a value:");
+int n = int.Parse(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine("The number's even");
+}
+else
+{
+    Console.WriteLine("The number's odd");
+}
