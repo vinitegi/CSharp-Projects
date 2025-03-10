5462b752a211a91f677f422a50fcc1b0b31bb5f6
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-10 14:17:45 -0300

    initial commit

---
+++ Exercicios/Lista002/ex003/ex003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Type two values");
+
+string[] value = Console.ReadLine().Split(' ');
+
+int a = int.Parse(value[0]);
+int b = int.Parse(value[1]);
+
+if (a % b == 0 || b % a == 0)
+{
+    Console.WriteLine("The numbers are multiples");
+}
+else
+{
+    Console.WriteLine("The numbers aren't multiples");
+}
