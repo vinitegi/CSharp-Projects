3bdd257fe91c23f61874522f828aa0e39108f01e
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-10 13:50:27 -0300

    initial commit

---
+++ Aula006/Aula006/Program.cs
@@ -0,0 +1,10 @@
+﻿int x = 10;
+
+Console.WriteLine("Good Morning");
+
+if (x < 5)
+{
+    Console.WriteLine("Good Afternoon");
+}
+
+Console.WriteLine("Good Night");
---
+++ Exercicios/Lista002/ex001/ex001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Type a value: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 0)
+{
+    Console.WriteLine("The value is negative");
+} else
+{
+    Console.WriteLine("The value is positive");
+}
