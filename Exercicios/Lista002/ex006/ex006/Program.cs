25d61d84e9231dcdc70fd886bad27d5e9ab61625
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-17 14:36:59 -0300

    initial commit

---
+++ Exercicios/Lista002/ex006/ex006/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+if (numero < 0 || numero > 100)
+{
+    Console.WriteLine("Numero fora do intervalo");
+}
+else if (numero <= 25.0)
+{
+    Console.WriteLine("Intervalo [0,25]");
+}
+else if (numero <= 50.0)
+{
+    Console.WriteLine("Intervalo (25,50]");
+}
+else if (numero <= 75.0)
+{
+    Console.WriteLine("Intervalo (50,75]");
+}
+else
+{
+    Console.WriteLine("Intervalo (75,100]");
+}
