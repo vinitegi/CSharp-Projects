4f8b0b195090c6004d5e8f2b20b5213d476db24e
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-03-17 14:26:39 -0300

    initial commit

---
+++ Exercicios/Lista002/ex005/ex005/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+Console.WriteLine("--------------------------------\n" +
+                  "CODIGO       PRODUTO        VALOR\n" +
+                  "1            Hot-Dog        R4,00\n" +
+                  "2            X-Salada       R4,50\n" +
+                  "3            X-Bacon        R5,00\n" +
+                  "4            Torrada        R2,00\n" +
+                  "5         Refrigerante      R1,50\n");
+
+Console.WriteLine("Digite o codigo do produto desejado e a quantidade: ");
+
+
+string[] valores = Console.ReadLine().Split(' ');
+int codigos =  int.Parse(valores[0]);
+int qntd = int.Parse(valores[1]);
+
+double total;
+
+if (codigos == 1)
+{
+    total = qntd * 4.0;
+}
+else if (codigos == 2)
+{
+    total = qntd * 4.5;
+}
+else if (codigos == 3)
+{
+    total = qntd * 5.0;
+}
+else if (codigos == 4)
+{
+    total = qntd * 2.0;
+}
+else
+{
+    total = qntd * 1.5;
+}
+
+Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
