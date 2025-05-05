771d8123a8a02a7e02f6b18ad64a7965e458ec96
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-05-05 14:09:24 -0300

    initial commit

---
+++ Section 4/Aula009/Aula009/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using Prod; //namespace da classe -> 'Prod'
+
+Produtos p = new Produtos();
+
+Console.WriteLine("Digite os dados do produto:");
+
+Console.WriteLine("Nome:");
+    p.nome = Console.ReadLine();
+
+Console.WriteLine("Valor: ");
+    p.valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Estoque: ");
+    p.qntd = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados do produto: " + p);
---
+++ Section 4/Aula009/Aula009/Produtos.cs
@@ -0,0 +1,19 @@
+﻿namespace Prod
+{
+    class Produtos
+    {
+        public string nome;
+        public double valor;
+        public int qntd;
+
+        public double ValorTotalEstoque()
+        {
+            return (valor * qntd);
+        }
+
+        public override string ToString()
+        {
+            return nome + ", R$ " + valor + ", " + qntd + " unidades em estoque";
+        }
+    }
+}
