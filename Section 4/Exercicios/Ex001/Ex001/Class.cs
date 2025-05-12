d6e7c6bc07a197e36779e7cc67553c368c9d2edc
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-05-12 14:59:23 -0300

    initial commit

---
+++ Section 4/Exercicios/Ex001/Ex001/Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClasseEx001
+{
+    public class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+        public static Pessoa MaiorIdade(Pessoa P1, Pessoa P2)
+        //adionando o 'static' o método MaiorIdade pode ser chamado diretamente pela classe Pessoa
+        {
+            if (P1.idade > P2.idade)
+            {
+                return P1;
+            } else
+            { 
+                return P2;
+            }
+
+        }
+    }
+}
---
+++ Section 4/Exercicios/Ex001/Ex001/Main.cs
@@ -0,0 +1,21 @@
+﻿using ClasseEx001;
+
+Pessoa P1 = new Pessoa();
+Pessoa P2 = new Pessoa();
+
+//Pessoa 1
+Console.WriteLine("Digite o nome da pessoa 1:");
+    P1.nome = Console.ReadLine();
+Console.WriteLine("Digite a idade da pessoa 1:");
+    P1.idade = int.Parse(Console.ReadLine());
+
+//Pessoa 2
+Console.WriteLine("Digite o nome da pessoa 2:");
+    P2.nome = Console.ReadLine();
+Console.WriteLine("Digite a idade da pessoa 2:");
+    P2.idade = int.Parse(Console.ReadLine());
+
+//Verificação
+
+Pessoa MaisVelha = Pessoa.MaiorIdade(P1, P2);
+Console.WriteLine($"A pessoa mais velha é {MaisVelha.nome}, com {MaisVelha.idade} anos de idade.");
