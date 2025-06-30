3004adf89ac41c1e1d46a1ef20afb1bed823658c
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-06-30 13:54:08 -0300

    Third commit

---
+++ Section 4/Aula008/Program.cs
@@ -0,0 +1,24 @@
+﻿using Aula008;
+using System.Globalization;
+
+Triangulo X, Y;
+X = new Triangulo();
+Y = new Triangulo();
+
+Console.WriteLine("Entre com as medidas do triangulo X:");
+X.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+X.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+X.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double areaX = X.Area();
+Console.WriteLine("Área de X = " + areaX);
+
+
+Console.WriteLine("Entre com as medidas do triangulo Y:");
+Y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double areaY = Y.Area();
+Console.WriteLine("Área de Y = " + areaY);
+Console.ReadKey(); // ← segura o console
---
+++ Section 4/Aula008/Triangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula008
+{
+    internal class Triangulo
+    {
+        public double A, B, C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            
+            return raiz;
+        }
+
+    }
+}
