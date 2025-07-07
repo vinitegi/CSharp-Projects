4631b0e99832073fe440fe3a11b4dc4520cfaf30
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-07-07 09:13:12 -0300

    First commit

---
+++ Section 4/Aula009/Produtos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Valor;
+        public int Qntd;
+
+        public double ValorTotalEstoque()
+        {
+            return Valor * Qntd;
+        }
+
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Valor.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Qntd
+                + " unidades, Total: $ "
+                + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
+
---
+++ Section 4/Aula009/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Estoque;
+
+Produto p = new Produto();
+
+Console.WriteLine("Entre os dados do produto: ");
+
+Console.WriteLine("Nome: ");
+p.Nome = Console.ReadLine();
+
+Console.WriteLine("Valor: ");
+p.Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Quantidade no estoque: ");
+p.Qntd = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Dados do produto: " + p);
+Console.ReadKey();
