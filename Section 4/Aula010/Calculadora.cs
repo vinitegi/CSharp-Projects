84ac45592c4b054d372d7be8977e27353870746e
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-07-08 15:32:40 -0300

    first commit

---
+++ Section 4/Aula010/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Curso
+{
+    public class Calculadora
+    {
+        public static double Pi = 3.14;
+        public static double Circunferencia(double raio)
+        {
+           return 2 * Pi * raio;
+        }
+        
+
+        public static double Volume(double raio)
+        {
+           return (4.0 / 3.0 * Pi) * Math.Pow(raio, 3);
+        }
+
+    }
+}
+
---
+++ Section 4/Aula010/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using Curso;
+
+
+Console.Write("Entre o valor do raio: ");
+double Raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double circ = Calculadora.Circunferencia(Raio);
+double vol = Calculadora.Volume(Raio);
+
+
+Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+Console.ReadKey();
