0de82a7d737accd51819232a8f916c6a02b35647
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-07-08 14:25:32 -0300

    first commit

---
+++ Section 4/Exercicios/Ex003/Boletim.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace PerfilAluno
+{
+    public class Aluno
+    {
+        public string Nome;
+        public double N1, N2, N3;
+
+        public void Avaliar()
+        {
+            double NotaFinal = N1 + N2 + N3;
+
+            Console.WriteLine("Nota Final = " + NotaFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+            if (NotaFinal > 60)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram {0} pontos", (60 - NotaFinal).ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+
+    }
+}
+
---
+++ Section 4/Exercicios/Ex003/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using PerfilAluno;
+
+Aluno estudante = new Aluno();
+
+Console.Write("Nome do aluno: ");
+    estudante.Nome = Console.ReadLine();
+
+do
+{
+    Console.WriteLine("Digite as três notas do aluno:");
+    estudante.N1 = double.Parse(Console.ReadLine());
+    estudante.N2 = double.Parse(Console.ReadLine());
+    estudante.N3 = double.Parse(Console.ReadLine());
+
+    if (estudante.N1 > 30 || estudante.N2 > 35 || estudante.N3 > 35)
+    {
+        Console.WriteLine("Alguma nota excede o valor máximo permitido. Digite novamente.\n");
+    }
+}
+while (estudante.N1 > 30 || estudante.N2 > 35 || estudante.N3 > 35);
+
+Console.WriteLine();
+estudante.Avaliar();
+Console.ReadKey();
+
+
+
