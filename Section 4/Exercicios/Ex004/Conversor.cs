13c81938862f9319a3bbac2e02bed2e38adb56df
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-07-15 13:53:18 -0300

    last commit

---
+++ Section 4/Exercicios/Ex004/Conversor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class ConversorDeMoeda
+{
+	public static double Iof = 6.0;
+
+	static public double Calculadora(double quantidade, double cotacao)
+	{
+		double total = quantidade * cotacao;
+		return total + total * Iof / 100;
+	}
+}
---
+++ Section 4/Exercicios/Ex004/Main.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+Console.Write("Digite a cotação atual do dolar: ");
+    double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Digite quantos dolares você irá comprar: ");
+    double quantidade  = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double result = ConversorDeMoeda.Calculadora(quantidade, cotacao);
+
+Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+Console.ReadKey();
