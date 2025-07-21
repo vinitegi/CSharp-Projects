edd0d775bb162525a8ca7b94b4a5de5224291417
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-07-21 14:46:00 -0300

    section 5 - last commit

---
+++ Section 5/Exercicios/Ex001/Class.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bank
+{
+    class BankAccount
+    {
+        //propriedades
+        public int ID { get; private set; } //private set -> o ID nao podera ser alterado
+        public string Titular { get; set; }
+        public double Saldo { get; private set; } //private -> so sera alterado por meio de funcoes, nao livremente
+
+        private const double TaxaSaque = 5.00;
+
+        //contrutores
+        public BankAccount(int iD, string titular)
+        {
+            ID = iD;
+            Titular = titular;
+        }
+        public BankAccount(int iD, string titular, double depositoInicial) : this(iD, titular)
+        {
+            Depositar(depositoInicial);
+        }
+
+
+        //membros
+        public void Depositar (double valor)
+        {
+            Saldo += valor;
+        }
+        public void Sacar(double valor)
+        {
+            Saldo -= (valor + TaxaSaque);
+        }
+
+
+        //tostring
+        public override string ToString()
+        {
+            return "Conta" 
+                + ID + ", Titular: "
+                + Titular + ", Saldo: "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture)
+                + "\n";
+        }
+    }
+}
+
---
+++ Section 5/Exercicios/Ex001/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Bank;
+
+Console.Write("Entre o número da conta: ");
+int Id = int.Parse(Console.ReadLine());
+
+Console.Write("Entre o titular da conta: ");
+string Titular = Console.ReadLine();
+
+Console.Write("Haverá depósito inicial (s/n)? ");
+char Resp = char.Parse(Console.ReadLine());
+
+BankAccount conta;
+
+if (Resp == 's' || Resp == 'S')
+{
+    Console.Write("Entre o valor de depósito inicial: ");
+    double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    conta = new BankAccount(Id, Titular, depositoInicial);
+
+    Console.WriteLine("\nDados da conta:");
+    Console.WriteLine(conta);
+}
+else
+{
+    conta = new BankAccount(Id, Titular);
+    Console.WriteLine(conta);
+}
+
+Console.WriteLine("Entre um valor para o deposito: ");
+double Valor = double.Parse((Console.ReadLine()));
+conta.Depositar(Valor);
+Console.WriteLine("Dados da conta atualizados: ");
+Console.WriteLine(conta);
+
+Console.WriteLine("Entre um valor para o saque: ");
+Valor = double.Parse((Console.ReadLine()));
+conta.Sacar(Valor);
+Console.WriteLine("Dados da conta atualizados: ");
+Console.WriteLine(conta);
+
+Console.ReadKey();
