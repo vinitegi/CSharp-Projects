394824463982cad58cbfa51dd9d95cdba1e621c3
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-07-28 10:29:15 -0300

    Section 6 | Ex001 - second commit

--- Section 6/Exercicios/Ex001/Main.cs
+++ Section 6/Exercicios/Ex001/Main.cs
@@ -1,44 +1,51 @@
-﻿using System.Globalization;
-using Ex001;
+﻿using Ex001;
 
 Aluguel[] Quartos = new Aluguel[10];
 
-Console.Write("Quantos estudantes vao alugar quartos? ");
-    int n = int.Parse(Console.ReadLine());
+Console.Write("How many rooms gonna be rented? ");
+int Quantidade = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < n; i++)
+for (int i = 0; i < Quantidade; i++)
 {
-    Console.WriteLine("\nQuartos disponíveis:");
-    for (int j = 0; j < Quartos.Length; j++)
-    {
-        if (Quartos[j] == null)
-        {
-            Console.WriteLine($"Quarto {j} está disponível.");
-        }
-    }
-
-    Console.WriteLine($"\nAluguel #{i}:");
-
+    Console.WriteLine($"\nAluguel #{i + 1}:");
     Console.Write("Nome: ");
         string nome = Console.ReadLine();
     Console.Write("Email: ");
         string email = Console.ReadLine();
 
-    int Escolha;
+    int RoomChoice;
 
-    while (true) //verificação para saber se o quarto esta vazio ou nao -> evita sobrescrição
+    while (true) //vai rodar ate ser falso, ou ate o 'break'
     {
-        Console.Write("Escolha o seu quarto (0 a 9): ");
-            Escolha = int.Parse(Console.ReadLine());
-
-        if (Quartos[Escolha] == null)
+        Console.WriteLine("\n\nQuartos disponíveis:\n");
+        Console.WriteLine("------------------------------------------------");
+        for (int j = 0; j < Quartos.Length; j++)
         {
-            break; //o quarto esta vazio, pode alugar
-        } 
+            if (Quartos[j] == null)
+            {
+                Console.WriteLine($"Quarto {j} está disponível.");
+            }
+        }
+        Console.WriteLine("------------------------------------------------");
+
+        Console.Write("Escolha um quarto de 0 a 9: ");
+        RoomChoice = int.Parse(Console.ReadLine());
+
+        if (RoomChoice < 0 || RoomChoice >= Quartos.Length) //verifica se a escolha esta entre o intervalo de 0 e 9
+        {
+            Console.WriteLine("Entrada invalida. Digite outro numero!");
+        }
+        else if (Quartos[RoomChoice] == null)
+        {
+            {
+                break; //quarto esta vazio, sai do 'while'
+            }
+        }
         else
         {
-            Console.WriteLine("Este quarto ja esta ocupado, escolha outro.");
+            Console.WriteLine("Este quarto ja esta ocupado, escolha outro");
         }
     }
-    Quartos[Escolha] = new Aluguel(nome, email);
+
+    Quartos[RoomChoice] = new Aluguel(nome, email);
 }
